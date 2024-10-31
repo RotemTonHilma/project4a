40699997afcd00a6e35b4d45ac5fa316bc39108b
RotemTon <rotemton@hilma.tech>
2024-10-31 14:12:35 +0200

    buttons component and textbox styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react';
 import Keyboard from './Components/Keyboard';
+import Buttons from "./Components/Buttons";
 
 function App() {
   const [textArr, setTextArr] = useState([]);
 
   const langArr = ["English", "עברית"];
   const [currentLang, setCurrentLang] = useState(langArr[0]);
-
+  let [capsOverride, setCapsOverride] = useState(false);
+  let [colorOverride, setColorOverride] = useState(false);
 
   let currentStyle = textArr.length ?
     {
@@ -20,33 +22,9 @@ function App() {
       color: "hotpink"
     }
 
-
-  let [capsOverride, setCapsOverride] = useState(false);
-  function toggleCapsOverride() {
-    setCapsOverride(prev => !prev);
-  }
-  let [colorOverride, setColorOverride] = useState(false);
-  function toggleColorOverride() {
-    setColorOverride(prev => !prev);
-  }
-
-
-  const colorArr = ["hotpink", "magenta", "mistyrose", "plum", "peachpuff"];
-  const fontStyleArr = ["serif", "sans-serif", "fantasy", "monospace"];
-  const fontSizeArr = [12, 16, 20, 28, 36];
-
-  function handlBackspace() {
-    setTextArr(prevArr => prevArr.slice(0, prevArr.length - 1));
-  }
-
-  function handleChangeLang(l) {
-    setCurrentLang(l);
-  }
-
-
   return (<div>
 
-    <main id="textbox">{textArr.map(({ letter, color, font, size }, index,) => {
+    <main style={{ minHeight: "100px", maxWidth: "1000px", border: "1px solid pink", marginBottom: "20px" }} id="textbox">{textArr.map(({ letter, color, font, size }, index,) => {
       return (letter === "enter" ? <br key={index}></br>
         : <span key={index}
           style=
@@ -56,42 +34,17 @@ function App() {
     })}
     </main>
 
-    <div id="allButtons">
-      <div id="keyboard"> <Keyboard textArr={textArr} setTextArr={setTextArr} currentStyle={currentStyle} language={currentLang} /> </div>
-      <div id="languages">
-        {langArr.map((lang) => {
-          return <button key={lang} onClick={() => handleChangeLang(lang)}>{lang}</button>;
-        })}
-      </div>
-      <div id="spacing">
-        <button onClick={handlBackspace}>BackSpace</button>
-      </div>
-      <div id="size">
-        <h1>size</h1>
-        {fontSizeArr.map((size, index) => {
-          return <button key={index} onClick={() => currentStyle.fontSize = size}>{size}</button>;
-        })}
-      </div>
-      <div id="font">
-        <h1>font</h1>
-        {fontStyleArr.map((style, index) => {
-          return <button key={index} onClick={() => currentStyle.fontFamily = style}>{style}</button>;
-        })}
-      </div>
-
-      <div id="color">
-        <h1>color</h1>
-        {colorArr.map((color, index) => {
-          return <button key={index} onClick={() => currentStyle.color = color}>{color}</button>;
-        })}
-      </div>
-      <div id="special">
-        <div>
-          <button onClick={toggleCapsOverride}> Caps all text</button>
-          <button onClick={toggleColorOverride}> Color all text</button>
-        </div>
-      </div>
+    <div id="keyboard">
+      <Keyboard textArr={textArr} setTextArr={setTextArr} currentStyle={currentStyle} language={currentLang} />
     </div>
+
+    <Buttons currentLang={currentLang} setCurrentLang={setCurrentLang}
+      capsOverride={capsOverride} setCapsOverride={setCapsOverride}
+      colorOverride={colorOverride} setColorOverride={setColorOverride}
+      currentStyle={currentStyle}
+      langArr={langArr}
+      textArr={textArr} setTextArr={setTextArr}
+    />
 
   </div>);
 };
