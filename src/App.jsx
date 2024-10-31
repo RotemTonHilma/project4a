8aa187485878683ae707c9ed27211257e396bbc9
naamaWyler <naamawyler@hilma.tech>
2024-10-31 10:40:04 +0200

    nit imortant

--- src/App.jsx
+++ src/App.jsx
@@ -15,18 +15,12 @@ function App() {
       fontFamily: "serif",
       fontSize: 20,
       color: "hotpink"
-    };
-  console.log(currentStyle);
+    }
 
 
   const colorArr = ["hotpink", "magenta", "mistyrose", "plum"];
-<<<<<<< HEAD
-  const fontStyleArr = ["serif", "sans-serif", "Courier New", "Times New Roman"];
+  const fontStyleArr = ["Arial", "Georgia", "Courier New", "Times New Roman"];
   const fontSizeArr = [12,  16,  20,  28,  36];
-=======
-  const fontStyleArr = ["serif", "sans-serif", "fantasy", "monospace"];
-  const fontSizeArr = [12, 16, 20, 28, 36];
->>>>>>> c299c1ea85be96df672c353ecb1643b0ef144f00
 
 
   return (<div>
@@ -41,28 +35,28 @@ function App() {
     </main>
 
     <div id="allButtons">
-      <div id="keyboard"> <Keyboard textArr={textArr} setTextArr={setTextArr} currentStyle={currentStyle} /> </div>
-      <div id="languages"></div>
-      <div id="size">
+        <div id="keyboard"> <Keyboard textArr={textArr} setTextArr={setTextArr} currentStyle={currentStyle}/> </div>
+        <div id="languages"></div>
+        <div id="size">
         <h1>size</h1>
-        {fontSizeArr.map((size, index) => {
-          return <button key={index} onClick={() => currentStyle.fontSize = size}>{size}</button>;
-        })}
-      </div>
-      <div id="font">
-        <h1>font</h1>
-        {fontStyleArr.map((style, index) => {
-          return <button key={index} onClick={() => currentStyle.fontFamily = style}>{style}</button>;
-        })}
-      </div>
-
-      <div id="color">
-        <h1>color</h1>
-        {colorArr.map((color, index) => {
-          return <button key={index} onClick={() => currentStyle.color = color}>{color}</button>;
-        })}
-      </div>
-      <div id="special"></div>
+        {fontSizeArr.map((size, index)=>{
+                return <button key={index} onClick={()=>currentStyle.fontSize=size}>{size}</button>;
+            })}
+        </div>
+        <div id="font">
+            <h1>font</h1>
+        {fontStyleArr.map((style, index)=>{
+                return <button key={index} onClick={()=>currentStyle.fontStyle=style}>{style}</button>;
+            })}
+        </div>
+        
+        <div id="color">
+            <h1>color</h1>
+            {colorArr.map((color, index)=>{
+                return <button key={index} onClick={()=>currentStyle.color=color}>{color}</button>;
+            })}
+        </div>
+        <div id="special"></div>
     </div>
 
   </div>);
