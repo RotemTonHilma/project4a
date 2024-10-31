1f971cfcf44571ca19193895adf201e737139675
RotemTon <rotemton@hilma.tech>
2024-10-31 09:52:11 +0200

    keep previous letter style

--- src/App.jsx
+++ src/App.jsx
@@ -2,40 +2,48 @@ import { useState } from 'react';
 import Keyboard from './Components/Keyboard';
 
 function App() {
-    const [textArr, setTextArr]=useState([]);
-    let styleOverride = false
-    let currentStyle = {
-        fontFamily: "serif",
-        fontSize: 20,
-        color:"hotpink"
+  const [textArr, setTextArr] = useState([]);
+  let styleOverride = false;
+
+  let currentStyle = textArr.length ?
+    {
+      fontFamily: textArr[textArr.length - 1].font,
+      fontSize: textArr[textArr.length - 1].size,
+      color: textArr[textArr.length - 1].color
+    } :
+    {
+      fontFamily: "serif",
+      fontSize: 20,
+      color: "hotpink"
     }
-    let colorArr =["hotpink", "magenta", "mistyrose", "plum"]
 
-    return (<div>
 
-    <main id="textbox">{textArr.map(({letter, color, font, size},index)=>{
-        return <span key={index} 
-                    style={!styleOverride?{color, fontSize: size, fontFamily: font}
-                    :null}>
-            {letter}
-            </span>
+  let colorArr = ["hotpink", "magenta", "mistyrose", "plum"];
+
+  return (<div>
+
+    <main id="textbox">{textArr.map(({ letter, color, font, size }, index) => {
+      return <span key={index}
+        style={!styleOverride ? { color, fontSize: size, fontFamily: font }
+          : null}>
+        {letter}
+      </span>
     })}
     </main>
 
     <div id="allButtons">
-        <div id="keyboard"> <Keyboard textArr={textArr} setTextArr={setTextArr} currentStyle={currentStyle}/> </div>
-        <div id="languages"></div>
-        <div id="size"></div>
-        <div id="font"></div>
-        <div id="special"></div>
-        <div id="color">
-            {colorArr.map((color, index)=>{
-                return <button key={index} onClick={()=>currentStyle.color=color}>{color}</button>;
-            })}
-        </div>
+      <div id="keyboard"> <Keyboard textArr={textArr} setTextArr={setTextArr} currentStyle={currentStyle} /> </div>
+      <div id="languages"></div>
+      <div id="size"></div>
+      <div id="font"></div>
+      <div id="special"></div>
+      <div id="color">
+        {colorArr.map((color, index) => {
+          return <button key={index} onClick={() => currentStyle.color = color}>{color}</button>;
+        })}
+      </div>
     </div>
 
-    </div>);
-}
-
+  </div>);
+};
 export default App
--- src/Components/Keyboard.jsx
+++ src/Components/Keyboard.jsx
@@ -1,28 +1,28 @@
-function Keyboard({textArr,setTextArr, currentStyle}){
-    const letterArr=["a", "b","c"];
+function Keyboard({ textArr, setTextArr, currentStyle }) {
+    const letterArr = ["a", "b", "c"];
 
-   function createLetterObj(letter, size, font, color){
-    return{
-        letter,
-        size,
-        font,
-        color,
+    function createLetterObj(letter, size, font, color) {
+        return {
+            letter,
+            size,
+            font,
+            color,
+        }
     }
-   }
 
-    function handleLetterClick(letter, color, font, size){
-        setTextArr(prevArr=>{
-            console.log(currentStyle);
-            return [...prevArr,createLetterObj(letter
-            , size, font, color
-        )]}
+    function handleLetterClick(letter, color, font, size) {
+        setTextArr(prevArr => {
+            return [...prevArr, createLetterObj(letter
+                , size, font, color
+            )]
+        }
         );
     }
 
     return (
         <>
             {letterArr.map((letter) => {
-                return <button key={letter} onClick={() => handleLetterClick(letter,currentStyle.color, currentStyle.fontFamily,currentStyle.fontSize)}>{letter}</button>
+                return <button key={letter} onClick={() => handleLetterClick(letter, currentStyle.color, currentStyle.fontFamily, currentStyle.fontSize)}>{letter}</button>
             })}
         </>
     );
